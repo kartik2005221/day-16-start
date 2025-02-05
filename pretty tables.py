7fb98a539eb623c1d81e7eab881f3b01a9fd678c
Kartik <kartik2005221@proton.me>
2025-02-05 22:20:18 +0530

    all repos for practice here, of 100 days of code

---
+++ pretty tables.py
@@ -0,0 +1,8 @@
+from prettytable import PrettyTable
+
+table = PrettyTable()
+table.add_column("adfhfh", ["b", "c", "d"])
+table.add_column("egfjhfg", ["f", "g", "h"])
+table.align = "r"
+
+print(table)
---
+++ turtle.py
@@ -0,0 +1,12 @@
+from turtle import Turtle, Screen
+
+turtle = Turtle()
+print(turtle)
+turtle.shape("turtle")
+turtle.color("DarkOrange1")
+turtle.forward(100)
+
+screen = Screen()
+print(screen.canvwidth)
+screen.exitonclick()
+
